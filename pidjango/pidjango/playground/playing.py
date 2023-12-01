616fc0337f48385040f8f69bc24dadf12f826376
Kiril Voigtlaender <kiril.voigtlaender@ru.nl>
2023-12-01 14:10:03 +0100

    set up of the structure Co-authored-by: Quillettes, K.E.E.S. de (Kareem) <kareem.dequillettes@ru.nl>

---
+++ pidjango/pidjango/playground/playing.py
@@ -0,0 +1,16 @@
+import mido
+from adafruit_servokit import ServoKit
+from .midi_functions.map_midi_into_letter import map_midi_into_letter
+from .midi_functions.map_midi_to_server import map_midi_to_server
+from .models import Midi_file
+def playing(file):
+    kit = ServoKit(channels=16)
+    #file = Midi_file . 
+    #fin = mido.MidiFile(file)
+    fin = mido.MidiFile('/home/guitar/Desktop/New Devices/Project/c14/sonata_1_1__c_iscenko.mid')
+    for message in fin.play():
+        if message.type in ['note_on', 'note_off']:
+            outgoing_letter = map_midi_into_letter(message.note)
+            outgoing_turned_servo = map_midi_to_server(outgoing_letter,kit)
+    
+    return 0
